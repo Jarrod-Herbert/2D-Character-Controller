2f21a878ff43d51582ce9536853ca74ece43ee70
Jarrod Herbert <herbert.jarrod@gmail.com>
2022-08-13 17:53:26 -0400

    Added player input actions and jump

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private PlayerInputActions _playerInputActions;
+    private InputAction _movement;
+
+    private Rigidbody2D _rb;
+    
+    [SerializeField] private float _jumpForce;
+
+    private void Awake()
+    {
+        _playerInputActions = new PlayerInputActions();
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        _movement = _playerInputActions.Player.Movement;
+        _movement.Enable();
+        
+        _playerInputActions.Player.Jump.performed += DoJump;
+        _playerInputActions.Player.Jump.Enable();
+    }
+
+    private void DoJump(InputAction.CallbackContext obj)
+    {
+        _rb.AddForce(Vector3.up * _jumpForce);
+    }
+
+    private void OnDisable()
+    {
+        _movement.Disable();
+        _playerInputActions.Player.Jump .Disable();
+    }
+
+    private void FixedUpdate()
+    {
+        // Read movement input
+    }
+}
