e1cbd58539cc66817e2419a51c8613feaa03cfb6
Jarrod Herbert <herbert.jarrod@gmail.com>
2022-08-14 10:57:37 -0400

    Movement refactor to CoreComponent

--- Assets/Scripts/Core/Movement.cs
+++ Assets/Scripts/Core/Movement.cs
@@ -7,16 +7,31 @@ using UnityEngine.InputSystem;
 public class Movement : CoreComponent
 {
     [SerializeField] private float _jumpForce= 10f;
+    [SerializeField] private float _moveSpeed = 3f;
+    
     public Rigidbody2D RB { get; private set; }
+
+    private Player _player;
 
     private void Awake()
     {
         RB = GetComponentInParent<Rigidbody2D>();
+        _player = GetComponentInParent<Player>();
     }
 
     public void Jump(InputAction.CallbackContext obj)
     {
         Debug.Log("Jumped from Movement component!");
         RB.velocity = new Vector2(RB.velocity.x, RB.velocity.y + _jumpForce);
+    }
+
+    public void MoveHorizontal()
+    {
+        RB.velocity = new Vector2(_player.InputManager.Movement.ReadValue<Vector2>().x * _moveSpeed, RB.velocity.y);
+    }
+
+    private void FixedUpdate()
+    {
+        MoveHorizontal();
     }
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -9,19 +9,12 @@ public class Player : MonoBehaviour
     public Core Core { get; private set; }
     public PlayerInputManager InputManager { get; private set; }
     
-    [SerializeField] private float _jumpForce;
-    [SerializeField] private float _horizontalSpeed;
     [SerializeField] private Transform _groundCheck;
     [SerializeField] private LayerMask _groundLayer;
-
-    private Rigidbody2D _rb;
-
     private void Awake()
     {
         Core = GetComponentInChildren<Core>();
         InputManager = GetComponent<PlayerInputManager>();
-
-        _rb = GetComponent<Rigidbody2D>();
     }
     
     private bool IsGrounded()
@@ -29,12 +22,6 @@ public class Player : MonoBehaviour
         return Physics2D.OverlapCircle(_groundCheck.position, 0.1f, _groundLayer);
     }
 
-    private void FixedUpdate()
-    {
-        _rb.velocity = new Vector2(InputManager.Movement.ReadValue<Vector2>().x * _horizontalSpeed,
-            _rb.velocity.y);
-    }
-    
     private void OnDrawGizmosSelected()
     {
         Gizmos.DrawWireSphere(_groundCheck.position, 0.1f);
