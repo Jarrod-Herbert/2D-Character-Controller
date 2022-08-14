d457647e7d4a9c1ff1aa0a6812657a735f59f301
Jarrod Herbert <herbert.jarrod@gmail.com>
2022-08-13 20:29:33 -0400

    Refactored Player Input

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public PlayerInputManager InputManager { get; private set; }
+    
+    [SerializeField] private float _jumpForce;
+    [SerializeField] private float _horizontalSpeed;
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private LayerMask _groundLayer;
+
+    private Rigidbody2D _rb;
+
+    private void Awake()
+    {
+        InputManager = GetComponent<PlayerInputManager>();
+
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(_groundCheck.position, 0.1f, _groundLayer);
+    }
+
+    public void DoJump(InputAction.CallbackContext obj)
+    {
+        _rb.velocity = new Vector2(_rb.velocity.x, _rb.velocity.y + _jumpForce);
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = new Vector2(InputManager.Movement.ReadValue<Vector2>().x * _horizontalSpeed,
+            _rb.velocity.y);
+    }
+    
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(_groundCheck.position, 0.1f);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerInputManager.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputManager : MonoBehaviour
+{
+    public InputAction Movement { get; private set; }
+    
+    private PlayerInputActions _playerInputActions;
+    private Player _player;
+
+    private void Awake()
+    {
+        _player = GetComponentInParent<Player>();
+        _playerInputActions = new PlayerInputActions();
+    }
+    
+    private void OnEnable()
+    {
+        Movement = _playerInputActions.Player.Movement;
+        Movement.Enable();
+        
+        _playerInputActions.Player.Jump.performed += _player.DoJump;
+        _playerInputActions.Player.Jump.Enable();
+    }
+    
+    private void OnDisable()
+    {
+        Movement.Disable();
+        _playerInputActions.Player.Jump .Disable();
+    }
+}
