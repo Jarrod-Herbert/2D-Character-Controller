406669f82d69bb3708fa20514f67c100747a4230
Jarrod Herbert <herbert.jarrod@gmail.com>
2022-09-10 19:26:45 -0400

    can stand on tilemap now gg

---
+++ Assets/Sprites/Tileset/Grassy/grassy.tsx
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="grassy" tilewidth="16" tileheight="16" tilecount="376" columns="8">
+ <image source="Sprites/Grassy_Ground.png" width="128" height="752"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2" y="4" width="14" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="4" width="16" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="4" width="16" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="4" width="16" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="4" width="16" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="4" width="16" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="4" width="16" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="4" width="14" height="12"/>
+  </objectgroup>
+ </tile>
+</tileset>
